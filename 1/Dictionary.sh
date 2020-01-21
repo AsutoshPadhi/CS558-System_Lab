d78ac8bff16fbb3758316c5e467479d68e968fe0
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2020-01-21 22:03:06 +0530

    Add sort and file feature

--- 1/Dictionary.sh
+++ 1/Dictionary.sh
@@ -1,28 +1,53 @@
-# !/bin/sh
+# !/bin/bash
 
 printf "***************Welcome to Legendary Dictionary***************\n\n"
 
 # declare a hashmap datastructure called 'dict'
 declare -A dict
-
+while IFS='=' read word meaning
+   do
+    dict[$word]=$meaning
+     echo "$word : ${dict[$word]}"
+  done < file.txt
+    
 while :
 do
-    
+     a=0
     # Read the word and store it in the variable 'word'
     read -p "Enter the new word : " word
 
     # TODO: Read the meaning if needed
-
+      # [[ -v dict[$word] ]] && a=1 echo $word : ${dict[$word]};
+       [[ -v dict[$word] ]] && a=1 
+if [ $a == 0 ] 
+then 
     # Read the meaning
     read -p "Enter the meaning : " meaning
-    
+     
     # Insert into the dictionary
     dict[$word]=$meaning
-
     # Display the whole dictionary
+    echo "$word=${dict[$word]}">>file.txt
+    : '
     for key in "${!dict[@]}";
     do
         echo $key : ${dict[$key]};
     done
-    
-done
+    '
+    sort file.txt>copy.txt
+    cp copy.txt file.txt
+     
+    # echo >file.txt
+#      for key in "${!dict[@]}"; do
+#   printf '%s:%s\n' "$key" "${dict[$key]}"
+#      done | sort -t : -k 2n      
+fi
+   if [ $a == 1 ] 
+then 
+    echo $word : ${dict[$word]}
+    fi
+    # for key in "${!dict[@]}";
+    # do
+    #     echo "$key : ${dict[$key]}">>file.txt
+    # done
+ done
